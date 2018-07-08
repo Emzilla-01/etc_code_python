43246cf057cb335a42255ded746e30fd15c25085
zzmku <emma.mi.won.kay@gmail.com>
2018-07-08 14:00:04 -0400

    Messing around with github desktop windows...
    Changed link to "HOW TO DO THE GITHUB (The Absolute Basics)" by TheHappieCat on youtube.

--- Udacity Intro/take_a_break.py
+++ Udacity Intro/take_a_break.py
@@ -13,7 +13,7 @@ break_interval = 120
 iters = 0
 while iters <= 2:
     time.sleep(break_interval)
-    webbrowser.open('https://www.youtube.com/watch?v=evVcGk_6pd8')
+    webbrowser.open('https://www.youtube.com/watch?v=EUvmCuPjHD4')
     iters += 1
 print("All done!")
 os.listdir(r'C:\Users\orpha\AppData\Local\conda\conda\envs\py27')
